15e77318fc325f660e4d2498e8d929ae43c874b6
Qais Aziz <azizqais96@gmail.com>
2022-05-08 17:23:28 +0500

    Connect To Database

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api');
 const { schema } = require('joi/lib/types/object');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root1234',
+    database: 'product'
+});
+
+connection.connect(function(err){
+    if(err) return console.error('error: ' + err.message);
+
+    console.log('Connected to MySQL server.');
+})
+
 
 app.use(express.json());
 app.use('/api', apiRouter);
